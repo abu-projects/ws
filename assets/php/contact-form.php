0e0387eeaae9959c00305091bb27f8c39996af95
aboelkheir <43382908+aboelkheir@users.noreply.github.com>
2023-12-21 21:13:29 +0200

    q

--- assets/php/contact-form.php
+++ assets/php/contact-form.php
@@ -1,16 +1,49 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-    $to = 'yapepo@gmail.com'; //<-- Enter your E-Mail address here.
-    $subject = $_POST['subject'];
 
-    $body = "From: $name <br> E-Mail: $email <br> Message: <br> $message";
+// Check if the form was submitted
+if($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $headers  = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-    $headers .= 'From:' . $email. "\r\n";
-    $headers .= 'Cc:' . $email. "\r\n";
+  // Get the form data
+  $name = trim($_POST["name"]);
+  $email = trim($_POST["email"]);
+  $subject = trim($_POST["subject"]);
+  $message = trim($_POST["message"]);
 
-    mail($to, "New Message from Website: $subject", $body, $headers);
+  // Validate form data
+  if(empty($name) || empty($email) || empty($subject) || empty($message)) {
+    $error = "Please fill in all fields.";
+  } else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $error = "Invalid email address.";  
+  } else {
+  
+    // Set recipient email address
+    $recipient = "info@example.com";
+    
+    // Set email subject and headers
+    $subject = "Contact Form Submission: $subject";
+    $headers = "From: $name <$email>";
+    
+    // Build email content
+    $content = "Name: $name\n";
+    $content .= "Email: $email\n\n";
+    $content .= "Message:\n$message\n";
+        
+    // Send the email
+    $success = mail($recipient, $subject, $content, $headers);
+    
+    // Check if email was sent
+    if($success) {
+      $error = "Thanks! Your message has been sent.";  
+    } else {
+      $error = "Oops! Something went wrong, please try again.";
+    }
+    
+  }
+
+}
+
+// Return JSON response
+$response = array("error" => $error);
+echo json_encode($response);
+
 ?>
